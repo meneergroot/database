5e922d17dca84a4af3723da7f290760cdbded1e4
MG <53813521+meneergroot@users.noreply.github.com>
2024-01-06 20:50:06 +0000

    change

--- src/routes/(app)/album/[id]/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { getAlbum } from '$server';
-
-export const load = (async ({ fetch, params }) => ({
-	album: getAlbum(fetch, params.id)
-})) satisfies PageServerLoad;
--- src/routes/(app)/artist/[id]/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { getArtist, getArtistTopTracks, getArtistAlbums, getReleatedArtists } from '$server';
-
-export const load = (async ({ fetch, params }) => ({
-	artist: getArtist(fetch, params.id),
-	topTracks: getArtistTopTracks(fetch, params.id),
-	albums: getArtistAlbums(fetch, params.id, 10),
-	releatedArtists: getReleatedArtists(fetch, params.id)
-})) satisfies PageServerLoad;
